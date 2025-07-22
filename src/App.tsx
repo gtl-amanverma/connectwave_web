80a16db8a0945669fdac38fa01a864d4e597b9d3
gtl_amanverma <aman.verma@thegatewaycorp.co.in>
2025-07-22 10:51:28 +0530

    Implemented splash screen page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import SplashscreenPage from "./pages/splash_screen/page";
 
 function App() {
+  const [isDisplaySplashScreen, setIsDisplaySplashScreen] =
+    React.useState(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsDisplaySplashScreen(true);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isDisplaySplashScreen) {
+    return <SplashscreenPage />;
+  }
+
   return (
     <div>
-      <h1 className='text-4xl'>Connectwave</h1>
+      <h1 className="text-4xl">Connectwave</h1>
     </div>
   );
 }
---
+++ src/pages/splash_screen/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SplashscreenPage = () => {
+  return (
+    <div className="w-screen h-screen bg-[#001D48] flex items-center justify-center">
+      <h1
+        className="text-white text-3xl md:text-5xl cursor typewriter-animation"
+        style={{ textShadow: "4px 4px 8px rgba(255,255,255,0.5)" }}
+      >
+        Connectwave
+      </h1>
+    </div>
+  );
+};
+
+export default SplashscreenPage;
